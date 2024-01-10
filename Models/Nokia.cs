a4a625b5ed9b8b25e6f2fe5d843b06d9e37f8b88
DayPateis <dayanepateis@gmail.com>
2024-01-10 16:55:02 -0300

    Desafio concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        // Construtor para a classe Nokia
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Intalando {nomeApp} no nokia {Modelo} - {Imei} e sua memoria é de {Memoria} GB.");
+        
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Criando uma instância de Nokia
+        Nokia nokia = new Nokia("11 99857-9854", "Nokia106", "953847", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Mimo");
+
+        Console.WriteLine();
+
+        // Criando uma instância de iPhone
+        Iphone iphone = new Iphone("11 995578-9853", "iPhoneX", "268745",68);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Duolingo");
+
+        Console.WriteLine();
+    }
+}
